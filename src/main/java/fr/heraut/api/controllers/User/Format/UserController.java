fba6b1e9877a8f54b1dbf9716d86d920d746e0c0
sylvain joly <sylvain.joly00@orange.fr>
2019-10-04 16:53:06 +0200

    WIP user api : queryParams

--- src/main/java/fr/heraut/api/controllers/User/Format/UserController.java
+++ src/main/java/fr/heraut/api/controllers/User/Format/UserController.java
@@ -5,9 +5,13 @@ import fr.heraut.api.models.User;
 import fr.heraut.api.repositories.UserRepository;
 import fr.heraut.api.services.ResponseFormat.GenericError;
 import fr.heraut.api.services.ResponseFormat.GenericSuccess;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.ws.Response;
@@ -24,19 +28,23 @@ public class UserController {
     GenericSuccess genericSuccess;
 
 
-    UserController(UserRepository userRepository, GenericSuccess genericSuccess, GenericError genericError){
+    UserController(UserRepository userRepository, GenericSuccess genericSuccess, GenericError genericError) {
         this.userRepository = userRepository;
         this.genericError = genericError;
         this.genericSuccess = genericSuccess;
     }
 
     @GetMapping
-    public ResponseEntity usersList () {
+    public ResponseEntity usersList(@RequestParam MultiValueMap<String, String> queryParams) {
+        // TODO : WIP
+        System.out.println("queryPARAMS : " + queryParams);
+        Pageable firstPageWithTwoElements = PageRequest.of(0, 3);
+
+        // TODO -> doc / help : https://www.baeldung.com/spring-data-jpa-pagination-sorting
+        // TODO -> pagination + query params management
         return genericSuccess.formatSuccess(userRepository
-                .findAll());
+                .findAll(firstPageWithTwoElements));
     }
-
-
 
 
 }
