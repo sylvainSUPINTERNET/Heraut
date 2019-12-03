2471c7ed1d825e42bad65b15784d0b6c7409c5bc
sylvain joly <sylvain.joly00@orange.fr>
2019-12-03 12:17:00 +0100

    clear

--- src/main/java/fr/heraut/api/controllers/User/Format/UserController.java
+++ src/main/java/fr/heraut/api/controllers/User/Format/UserController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController()
