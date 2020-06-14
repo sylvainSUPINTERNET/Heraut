e462b35ef842e531f385b852b8f8e91be0f97ad5
sylvain joly <sylvain.joly00@orange.fr>
2020-06-14 12:17:51 +0200

    [UPDATE] ws for community

--- microservices/messagerie-service/index.js
+++ microservices/messagerie-service/index.js
@@ -14,18 +14,8 @@ server.app.use("/test",testRouter);
 server.WSS.on('connection', (ws,req) => {
     console.info("websocket connection open");
 
-    // if there is not data in redis, we initialise the redis to get announce
-    redis.get("community", function(err, reply) {
-        if (err) throw err;
-
-        if(reply === null) {
-            // no data so init the data table
-            redis.set('community', JSON.stringify([]), redis.print);
-        }
-    });
-
-
     ws.on('message', async (message) => {
+
         /*
         Community payload e.g
 
@@ -36,60 +26,32 @@ server.WSS.on('connection', (ws,req) => {
          */
 
         const msgJson = JSON.parse(message);
-        console.log("receivec")
-        console.log("receivec")
-        console.log("receivec")
-console.log(msgJson)
-
-
-        console.log("receivec")
-        console.log("receivec")
-        console.log("receivec")
-
+        console.log("[INPUTS] : ", msgJson);
         const {source, userId, username, data} = msgJson;
+        const keyTarget = `${userId}`;
 
         if(source === "community") {
+            redis.get(keyTarget, (err, reply ) => {
+                if(err) throw err;
 
-            redis.get('community', (err, communityArrObjectData) => {
-                if(communityArrObjectData !== null) {
-                    // If user exist replace this user by the new value else, create new entry
-                    let userAlreadyExist = JSON.parse(communityArrObjectData).filter(el => el.username === username);
-                    if(userAlreadyExist.length > 0) {
-                        JSON.parse(communityArrObjectData).forEach( (el, index) => {
-                            // Just update position of existing user in array
+                if(reply !== null){
+                    let dataAtKey = JSON.parse(reply);
+                    // get by key and update the value from that key
+                    dataAtKey.data.lon = data.lon;
+                    dataAtKey.data.lat = data.lat;
+                    redis.set(`${userId}`, JSON.stringify(dataAtKey) ,redis.print);
+                    ws.send({newEntry: dataAtKey});
+                } else {
+                    // Init key + value
+                    redis.set(`${userId}`, JSON.stringify(msgJson) ,redis.print);
+                    ws.send({newEntry: msgJson});
 
-                            if(el.username === username) {
-                                JSON.parse(communityArrObjectData)[index].data.lon = data.lon;
-                                JSON.parse(communityArrObjectData)[index].data.lat = data.lat;
-                            }
-
-
-                        });
-                        console.log("````````")
-                        console.log("````````")
-                        redis.set('community', JSON.stringify(JSON.parse(communityArrObjectData)));
-                        console.log(JSON.parse(communityArrObjectData))
-                        console.log("````````")
-                        console.log("````````")
-
-                    } else {
-                        redis.set('community', JSON.stringify([...JSON.parse(communityArrObjectData), msgJson]));
-                        console.log("user not exist");
-                        console.log("DB state")
-                        console.log(JSON.parse(communityArrObjectData))
-                    }
                 }
-            });
-
-
+            })
         }
-        console.log(source);
-        console.log(userId);
-        console.log(username);
-        console.log(data);
-    });
 
-    //ws.send('Hi there, I am a WebSocket server');
+
+    });
 });
 
 
